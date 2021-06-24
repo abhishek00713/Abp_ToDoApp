98581e0914731a8793647206d77cf8581cf76e29
Chayan Shrestha <chayanshrestha@live.com>
2021-06-24 15:39:01 +0545

    Code Clean up

--- DemoApp/aspnet-core/src/DemoApp.EntityFrameworkCore.DbMigrations/Migrations/20210624094815_addsFreshMigration.Designer.cs
+++ DemoApp/aspnet-core/src/DemoApp.EntityFrameworkCore.DbMigrations/Migrations/20210624094815_addsFreshMigration.Designer.cs
@@ -4,15 +4,17 @@ using DemoApp.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
 namespace DemoApp.Migrations
 {
     [DbContext(typeof(DemoAppMigrationsDbContext))]
-    partial class DemoAppMigrationsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210624094815_addsFreshMigration")]
+    partial class addsFreshMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
